92d6f3d691a2fd1d8dd4690327c342a9ef4d5979
Bijal Thanawala <72908+bijalthanawala@users.noreply.github.com>
2025-11-03 17:22:38 +1100

    Found a much easier way to achieve the same

--- which_dnf_package.sh
+++ which_dnf_package.sh
@@ -1,7 +1,7 @@
 #! /bin/env sh
 
 usage() {
-    echo "------------------------------------"
+    echo "---------------------------------------------------"
     echo "Usage:"
     echo "$0 <binary name>"
     echo ""
@@ -12,8 +12,8 @@ usage() {
     echo "Last metadata expiration check: 1:05:01 ago on Mon Nov  3 02:41:15 2025."
     echo "net-tools-0:2.0-0.64.20160912git.el9.x86_64"
     echo ""
-    echo "Note: Specify full binary name. Wildcards are not supported."
-    echo "------------------------------------"
+    echo "Note: Specify full binary name -or- wildcard within quotes"
+    echo "----------------------------------------------------"
     
 }
 
@@ -24,20 +24,8 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-dnf list --installed yum-utils >/dev/null 2>&1
-status=$? 
-if [ $status -ne 0 ]; then
-    echo "Package yum-utils is not installed."
-    echo "Please install package yum-utils and then re-run this script."
-    exit 1
-fi
 
-REPOQ_FULLPATH="/bin/repoquery"
-if [ ! -f "${REPOQ_FULLPATH}" ]; then
-    echo "Unexpected error: Command ${REPOQ_FULLPATH} not found."
-    exit 1
-fi
 
-binary=$1
-${REPOQ_FULLPATH} /bin/${binary} /sbin/${binary} /usr/bin/${binary} /usr/sbin/${binary}
+binary="$1"
+dnf provides /bin/${binary} /sbin/${binary}
 
