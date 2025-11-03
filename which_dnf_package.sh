751829294b0dbac30796550ed25871bd763c5f21
Bijal Thanawala <72908+bijalthanawala@users.noreply.github.com>
2025-11-04 00:08:14 +1100

    Added some explanation

--- which_dnf_package.sh
+++ which_dnf_package.sh
@@ -14,7 +14,7 @@ usage() {
     echo ""
     echo "Note: Specify full binary name -or- wildcard within quotes"
     echo "----------------------------------------------------"
-    
+
 }
 
 if [ -z "$1" ]; then
@@ -31,3 +31,36 @@ fi
 
 binary="$1"
 dnf provides /bin/${binary} /sbin/${binary}
+# Prefixes /bin/ and /sbin/ in the above commands are essential because of the dnf behaviour.
+# Without the prefixes, dns stops the search if a package name matches the specified name
+# and then we only get list of packages matching the name and not the files.
+#
+# e.g.
+#     dnf provides "nets*"
+# gives -->
+# Last metadata expiration check: 4:18:16 ago on Mon Nov  3 02:41:15 2025.
+# netsniff-ng-0.6.8-11.el9.x86_64 : Packet sniffing beast
+# Repo        : epel
+# Matched from:
+# Provide    : netsniff-ng = 0.6.8-11.el9
+# Provide    : netsniff-ng(x86-64) = 0.6.8-11.el9
+#
+# netstandard-targeting-pack-2.1-9.0.111-1.el9_6.x86_64 : Targeting Pack for NETStandard.Library 2.1
+# Repo        : appstream
+# Matched from:
+# Provide    : netstandard-targeting-pack-2.1 = 9.0.111-1.el9_6
+# Provide    : netstandard-targeting-pack-2.1(x86-64) = 9.0.111-1.el9_6
+#
+# while
+#     dnf provides /bin/nets* sbin/nets*
+# gives  -->
+# Last metadata expiration check: 10:22:58 ago on Mon Nov  3 02:41:15 2025.
+# net-tools-2.0-0.64.20160912git.el9.x86_64 : Basic networking tools
+# Repo        : baseos
+# Matched from:
+# Filename    : /usr/bin/netstat
+#
+# netperf-2.7.0-2.20210803git3bc455b.el9.x86_64 : Benchmark to measure the performance of many different types of networking
+# Repo        : epel
+# Matched from:
+# Filename    : /usr/bin/netserver
